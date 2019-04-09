9c92f90d3c7f103bdefd303d9a1dbab27d08f74b
bogdanjava <bogdanshishkin1998@gmail.com>
2019-04-09 21:45:24 +0300

    init

---
+++ src/main/kotlin/by/bogdan/bsuir/bsuirgraduationbackend/BsuirGraduationBackendApplication.kt
@@ -0,0 +1,11 @@
+package by.bogdan.bsuir.bsuirgraduationbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BsuirGraduationBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<BsuirGraduationBackendApplication>(*args)
+}
---
+++ src/test/kotlin/by/bogdan/bsuir/bsuirgraduationbackend/BsuirGraduationBackendApplicationTests.kt
@@ -0,0 +1,16 @@
+package by.bogdan.bsuir.bsuirgraduationbackend
+
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit4.SpringRunner
+
+@RunWith(SpringRunner::class)
+@SpringBootTest
+class BsuirGraduationBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
